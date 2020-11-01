abb3d3690338aa064fa66d09acb2a4f0d6795c1d
Green-Pea-Ai <yachtboatfor@gmail.com>
2020-11-01 23:58:14 +0900

    201101 Sun 23:58, 리액트 리마인드

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import Counter from './components/Counter'
-
+import Hello from './components/hello/Hello'
+import Counter2 from './components/counter/Counter'
 
 // const는 function 타입이다.
-const App = () => <div><Counter/></div>
+const App = () => <div>
+  <Counter/>
+  <Hello/>
+  <Counter2/>
+
+</div>
   // return은 안보이지만 있다.
   // 빈값 return : undefined  
 
---
+++ src/components/todo/TodoList.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleTodoAction, deleteTodoAction } from './store/todoReducer'
+
+const TodoList = () => {
+
+    const todos  = useSelector(state => state.todos)
+    const dispatch = useDispatch()
+    const toggleTodo = todoId => dispatch(toggleTodoAction(todoId))
+    const deleteTodo = todoId => dispatch(deleteTodoAction(todoId))
+
+    return<>
+        <div>
+            { todos && todos.length === 0 && (
+                <p className = "alert alert-info">No Todos at the moment</p>
+            )}
+            { todos &&
+            todos.map(todo => (
+                <div key = { todo.id } className="row mb-1">
+                    <div className = "col-sm-2">
+                        <input
+                            type="checkbox"
+                            checked={ todo.complete }
+                            onChange={ toggleTodo.bind(null, todo.todoId)}
+                        />
+                        <span style={{ margin: '20px' }}>{ todo.name }</span>
+                        <button
+                            className="btn btn-danger"
+                            onClick={ deleteTodo.bind(null, todo.todoId)}
+                        >
+                            X
+                        </button>
+                    </div>
+            ))}
+        </div>
+    </>
+}
+export default TodoList
